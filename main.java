5ea8cafbab444f395305680f64020bd8a759766b
Juan Luis Garcia <jgarcia@martinexsa.com>
2018-09-20 08:53:59 -0600

    comments for work

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main {
     //private static final RecepcionDomicilioEvento eventos = new RecepcionDomicilioEvento();
     
     public static void main(String[] args) {
-        Pedido pedido = new Pedido();
+        Pedido pedido = new Pedido();//crear un pedido para ver si funciona
         Thread eventosThread = new Thread(new RecepcionDomicilioEvento(pedido));
         eventosThread.start();
     }
