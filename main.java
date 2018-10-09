3d074a959d941f081bff677bbf351e4168250087
Juan Luis Garcia <jgarcia@martinexsa.com>
2018-10-08 21:26:05 -0600

    clean repo

--- main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    //private static final RecepcionDomicilioEvento eventos = new RecepcionDomicilioEvento();
-    
-    public static void main(String[] args) {
-        Pedido pedido = new Pedido();//crear un pedido para ver si funciona
-        Thread eventosThread = new Thread(new RecepcionDomicilioEvento(pedido));
-        eventosThread.start();
-    }
-}
