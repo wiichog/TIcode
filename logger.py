c7fcf248d542fe398b9d041fe84ce50345ee4fe3
Hugo Noriega <13341708+juiceHN@users.noreply.github.com>
2018-10-16 00:14:53 -0600

    logger service added

---
+++ logger.py
@@ -0,0 +1,47 @@
+import logging
+import uuid
+
+logging.basicConfig(filename="system.log",
+                    format='%(message)s on %(asctime)s ',
+                    filemode='a')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def writeLog(category, message, gennedID):
+    if category == 1:
+        category = 'Info   '
+    elif category == 2:
+        category = 'Start  '
+    elif category == 3:
+        category = 'Finish '
+    elif category == 4:
+        category = 'Error  '
+    else:
+        category = 'Warning'
+    message = category + ':'+' id: '+str(gennedID)+' : '+message
+    logger.info(message)
+
+def queryLog(text):
+    messages = []
+    file = open('system.log', 'r')
+    lines = file.readlines()
+    for line in lines:
+        if text in line:
+            messages.append(line)
+    queryFile = open('result.txt', 'w')
+    result = 'found: '+str(len(messages))+' matches \n'
+    queryFile.write(result)
+    queryFile.writelines(messages)
+
+def genID():
+    return uuid.uuid1()
+
+""" writeLog(1, 'asaasa', genID())
+writeLog(2, 'asaasa', genID())
+writeLog(3, 'asaasa', genID())
+writeLog(1, 'asaasa', genID())
+writeLog(4, 'asaasa', genID())
+writeLog(1, 'asaasa', genID()) """
+  
+queryLog('74d6a7c2-d10a-11e8-8a34-48e24421f5bc')
